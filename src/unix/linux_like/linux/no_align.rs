3ce84f66225e144ba6ffcc53e15b901c49805162
Xiaotian Wu <wuxiaotian@loongson.cn>
2020-11-24 09:50:18 +0800

    add loongarch64 support

--- src/unix/linux_like/linux/no_align.rs
+++ src/unix/linux_like/linux/no_align.rs
@@ -4,6 +4,7 @@ macro_rules! expand_align {
             pub struct pthread_mutexattr_t {
                 #[cfg(any(target_arch = "x86_64",
                           target_arch = "powerpc64",
+                          target_arch = "loongarch64",
                           target_arch = "mips64",
                           target_arch = "s390x",
                           target_arch = "sparc64",
@@ -12,6 +13,7 @@ macro_rules! expand_align {
                 __align: [::c_int; 0],
                 #[cfg(not(any(target_arch = "x86_64",
                               target_arch = "powerpc64",
+                              target_arch = "loongarch64",
                               target_arch = "mips64",
                               target_arch = "s390x",
                               target_arch = "sparc64",
